c9648dc966e59619d746e57250077b2b010b916b
Oluwatobiloba Otun <bamidurooluwatobiloba@gmail.com>
2024-01-03 19:55:09 -0500

    done done

--- 0x01-python-if_else_loops_functions/100-print_tebahpla.py
+++ 0x01-python-if_else_loops_functions/100-print_tebahpla.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 x = 0
 for char in range(ord('z'), ord('a') - 1, -1):
-    print("{}".format(chr(char - i)), end="")
+    print("{}".format(chr(char - x)), end="")
     if x == 0:
         x = 32
     else:
