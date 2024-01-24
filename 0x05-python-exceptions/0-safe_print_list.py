705f6484b7b69368dbfb1d6f08fe51442b643e21
Tobi Otun <bamidurooluwatobiloba@gmail.com>
2024-01-24 08:11:09 -0800

    commit 0

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -5,7 +5,7 @@ def safe_print_list(my_list=[], x=0):
         try:
             print(my_list[val], end='')
             count += 1
-            except IndexError as e:
-                pass
-            print()
-            return count
+        except IndexError as e:
+            pass
+    print()
+    return count
