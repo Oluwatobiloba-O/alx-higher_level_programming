b9ba97908918aa8fc653c9da59e30c4ea8841228
root <root@LAPTOP-469I1VS1>
2024-04-10 21:04:52 -0400

    commit all

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,20 @@
+#!/usr/bin/node
+
+//function that increments and calls a function
+
+let myObject = {
+	type: 'object',
+	value: 12
+};
+console.log(myObject);
+
+myObject.incr = function () {
+	this.value++;
+};
+
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
---
+++ 0x12-javascript-warm_up/README.md
@@ -0,0 +1,5 @@
+0x12-javascript-warm_up task
+
+Done by
+
+Oluwatobiloba Otun
