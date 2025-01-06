7f458fa6294f210c7479a1419415320dbee071f3
root <root@LAPTOP-469I1VS1>
2025-01-06 15:44:18 -0500

    commit all

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -8,12 +8,12 @@
 import sys
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-from model_state import Base, State
+from model_state import State
 
 if __name__ == "__main__":
     engine = create_engine("mysql+mysqldb://{}:{}@localhost:3306/{}"
                            .format(sys.argv[1], sys.argv[2], sys.argv[3])),
-    Base.metadata.create_all(engine)
+    engine = create_engine(engine)
     Session = sessionmaker(bind=engine)
     session = Session()
     for instance in session.query(State).filter(State.name.like('%a%')):
